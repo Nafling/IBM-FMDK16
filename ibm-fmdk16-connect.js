982688203cb221d42b1fb1d2bb8b798b0df2cd73
Bolding1729 <Bolding@bjarkes-mbp.prv.dk.ibm.com>
2016-06-06 13:50:42 +0200

    Clean-up + ibm-fmdk16-connect added instead

--- ibm-fmdk16-connect.js
+++ ibm-fmdk16-connect.js
@@ -1,7 +1,22 @@
+process.stdout.write('\033c'); //clear terminal
+
 var Client = require('ibmiotf');
 var sphero = require("sphero");
-orb = sphero("f2:97:44:8f:3f:92"); //blue BB-8
-//orb = sphero("D3:27:B8:27:F1:06"); //red BB-8
+var orbParameter = process.argv[2];
+
+switch(orbParameter){
+    case "blue":
+        orb = sphero("f2:97:44:8f:3f:92"); //blue BB-8
+        console.log("Setting up MQTT-connection to Blue BB-8...");
+        break;
+    case "red":
+        orb = sphero("D3:27:B8:27:F1:06"); //red BB-8
+        console.log("Setting up MQTT-connection to Red BB-8...");
+        break;
+    default:
+        console.log("ERROR: Connect to Blue or Red? Example: sudo node fmdk16-connect.js blue");
+        process.exit(1);
+};
 
 var speed = 0;
 var direction = 0;
@@ -9,7 +24,7 @@ var calibration = false;
 
 var config = {
     "org" : "neia69",
-    "id" : "BLUE",
+    "id" : "" + orbParameter.toUpperCase() + "",
     "type" : "BB-8",
     "auth-method" : "token",
     "auth-token" : "FiskErSundt"
@@ -18,6 +33,7 @@ var config = {
 var deviceClient = new Client.IotfDevice(config);
 
 orb.connect(function() {
+    onsole.log("BB-8 Connected!");
     orb.color("green");
     orb.detectCollisions();
     orb.on("collision", function(data) {
@@ -34,8 +50,11 @@ orb.connect(function() {
     deviceClient.connect();
 });
 
+deviceClient.connect();
+
 deviceClient.on('connect', function () {
-    console.log("Wohoo connected!!");
+    console.log(config);
+    console.log("Connected to IBM Watson IoT Platform");
     deviceClient.publish("status","json",'{"d" : { "cpu" : 60, "mem" : 50 }}');
 });
 
